ed238ef831b7d63dc3f9e7454c013ffe516552ee
lang96 <1191101295@student.mmu.edu.my>
2021-01-03 06:31:09 +0800

    Arif

--- src/admin.java
+++ src/admin.java
@@ -1,5 +1,47 @@
 public class admin {
 
+    public void viewMaster() {
+
+        // This function allows admin to view the master visit history of the entire system
+        // The format is as below :
+        // No |    Date    |   Time   | Customer |  Shop  |
+        // 1. | 2020-12-01 | 15:10:15 |   Ali    |  Tesco |
+
+    }
+
+    public void viewCustomer() {
+
+        // This function allows admin to view the details of all customers registered in the system.
+        // Details of customers follows from customer.java
+        // The details of each customer which are generated from registration are stored in a json file and are -
+        // - retrieved in this java module.
+
+    }
+
+    public void viewShop() {
+
+        // This function allows admin to view the details of all shops within the system.
+        // Details of customers follows from shop.java
+        // The details of each shop are stored in a json file and are retrieved in this java module.
+
+    }
+
+    public void flagCustomer() {
+
+        // This function allows admin to flag a customer as a positive case of CoViD-19.
+        // With this function deployed, the flagShop() function automatically deploys as well.
+
+    }
+
+    public void flagShop() {
+
+        // This function allows admin to flag a shop as a zone of CoViD-19 positive cases, thus automatically -
+        // - flagging anyone who are considered close contacts (within the regulations set in customer class) as -
+        // CoViD-19 positive cases.
+
+    }
+
+
     // Test main module only
     public static void main(String[] args) {
         System.out.println("this is just to test branch commit and push");
--- src/mainApp.java
+++ src/mainApp.java
@@ -1,5 +1,32 @@
 public class mainApp {
+
+    /* NOTE : DELETE PRIOR TO ZIPPING AND SUBMISSION
+
+    Program Requirements :
+
+    1. Never clear screen - Accomplished using line-by-line console action or GUI (preferably the latter)
+
+    2. Pre-load with 5 customers (when the program starts) - Accomplished using json file to store data
+
+    3. Pre-load with 4 shops - Accomplished using json file to store data
+
+    4. Have a feature to add 30 random visits to the master visit history (random shops, random
+       visitors, time ranging from yesterday until current system time). Sort the master visit history
+       by date and time. - Accomplished with a function
+
+    Final Submission Requirement :
+
+    1. Java source code. Make sure the code can be compiled and run. [X]
+
+    2. Java documentation (Javadoc) for a class. Choose a class that have methods with parameter
+    and return type. [X]
+
+    3. A file named Members.txt that lists down the group members’ ID, name, and contribution. [~]
+
+     */
+
     public static void main(String[] args) {
 
     }
+
 }
