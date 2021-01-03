862d144c9f0750bd841658c55520607b76f2dd74
lang96 <1191101295@student.mmu.edu.my>
2021-01-03 10:24:04 +0800

    Arif

--- src/admin.java
+++ src/admin.java
@@ -1,28 +1,158 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class admin {
 
-    public void viewMaster() {
+    public static void viewMaster() {
 
-        // This function allows admin to view the master visit history of the entire system
-        // The format is as below :
-        // No |    Date    |   Time   | Customer |  Shop  |
-        // 1. | 2020-12-01 | 15:10:15 |   Ali    |  Tesco |
+        // This function allows admin to view the master visit history of the entire system.
+
+        // Reading JSON file \\
+
+        JSONParser jsonParser = new JSONParser();
+
+        // Parsing the contents of the JSON file
+
+        try (FileReader reader = new FileReader("C:\\Users\\clubberlang96\\IdeaProjects\\OOPDS_Assignment_1" +
+                "\\res\\visitHistory.json")) {
+
+            Object obj = jsonParser.parse(reader);
+
+            JSONObject visit = (JSONObject) obj;
+
+            JSONArray date = (JSONArray) visit.get("date");
+            JSONArray time = (JSONArray) visit.get("time");
+            JSONArray customer = (JSONArray) visit.get("customerName");
+            JSONArray shop = (JSONArray) visit.get("shopName");
+
+            System.out.printf("| %-2s | ", "No");
+            System.out.printf("%-10s | ", "Date");
+            System.out.printf("%-8s | ", "Time");
+            System.out.printf("%-15s | ", "Customer");
+            System.out.printf("%-15s |\n", "Shop");
+
+            for (int i = 0;i < customer.size();i++) {
+
+                System.out.printf("| %-2s | ", (i+1));
+                System.out.printf("%-10s | ", date.get(i));
+                System.out.printf("%-8s | ", time.get(i));
+                System.out.printf("%-15s | ", customer.get(i));
+                System.out.printf("%-15s |\n", shop.get(i));
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public void viewCustomer() {
+    public static void viewCustomer() {
 
         // This function allows admin to view the details of all customers registered in the system.
-        // Details of customers follows from customerData.java
+        // Details of customers follows from customer.java
         // The details of each customerData which are generated from registration are stored in a json file and are -
         // - retrieved in this java module.
 
+        // Reading JSON file \\
+
+        JSONParser jsonParser = new JSONParser();
+
+        // Parsing the contents of the JSON file
+
+        try (FileReader reader = new FileReader("C:\\Users\\clubberlang96\\IdeaProjects\\OOPDS_Assignment_1" +
+                "\\res\\customerData.json")) {
+
+            Object obj = jsonParser.parse(reader);
+
+            JSONObject customer = (JSONObject) obj;
+
+            JSONArray fName = (JSONArray) customer.get("fName");
+            JSONArray lName = (JSONArray) customer.get("lName");
+            JSONArray phoneNum = (JSONArray) customer.get("phoneNum");
+            JSONArray status = (JSONArray) customer.get("status");
+
+            System.out.printf("| %-2s | ", "No");
+            System.out.printf("%-20s | ", "Name");
+            System.out.printf("%-11s | ", "Phone");
+            System.out.printf("%-6s |\n", "Status");
+
+            for (int i = 0;i < phoneNum.size();i++) {
+
+                System.out.printf("| %-2s | ", (i+1));
+                System.out.printf("%-20s | ", fName.get(i) + " " + lName.get(i));
+                System.out.printf("%-11s | ", phoneNum.get(i));
+                System.out.printf("%-6s |\n", status.get(i));
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public void viewShop() {
+    public static void viewShop() {
 
         // This function allows admin to view the details of all shops within the system.
-        // Details of customers follows from shopData.java
+        // Details of customers follows from shop.java
         // The details of each shopData are stored in a json file and are retrieved in this java module.
+
+        // Reading JSON file \\
+
+        JSONParser jsonParser = new JSONParser();
+
+        // Parsing the contents of the JSON file
+
+        try (FileReader reader = new FileReader("C:\\Users\\clubberlang96\\IdeaProjects\\OOPDS_Assignment_1" +
+                "\\res\\shopData.json")) {
+
+            Object obj = jsonParser.parse(reader);
+
+            JSONObject shop = (JSONObject) obj;
+
+            JSONArray name = (JSONArray) shop.get("name");
+            JSONArray phoneNum = (JSONArray) shop.get("phoneNum");
+            JSONArray manager = (JSONArray) shop.get("manager");
+            JSONArray status = (JSONArray) shop.get("status");
+
+            System.out.printf("| %-2s | ", "No");
+            System.out.printf("%-20s | ", "Name");
+            System.out.printf("%-11s | ", "Phone");
+            System.out.printf("%-15s | ", "Manager");
+            System.out.printf("%-6s |\n", "Status");
+
+            for (int i = 0;i < phoneNum.size();i++) {
+
+                System.out.printf("| %-2s | ", (i+1));
+                System.out.printf("%-20s | ", name.get(i));
+                System.out.printf("%-11s | ", phoneNum.get(i));
+                System.out.printf("%-15s | ", manager.get(i));
+                System.out.printf("%-6s |\n", status.get(i));
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -50,10 +180,14 @@ public class admin {
 
     }
 
-
     // Test main module only
     public static void main(String[] args) {
-        System.out.println("this is just to test branch commit and push");
+        viewCustomer();
+        System.out.println("\n");
+        viewShop();
+        System.out.println("\n");
+        viewMaster();
+        System.out.println("\n");
     }
 
 }
